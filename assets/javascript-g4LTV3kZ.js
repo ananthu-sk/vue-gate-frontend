237491e290f3a11006f93ec3318c01085e2567d8
ananthu <ask1234560@gmail.com>
2025-11-23 15:19:59 +0100

    rename download to run

--- assets/javascript-g4LTV3kZ.js
+++ assets/javascript-g4LTV3kZ.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-p0crs1o4.js";import"./index-B7zzBjAB.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-RhGVD2zy.js";import"./index-CjRl7RQj.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.54.0(7c2310116c57517348bbd868a21139f32454be22)
  * Released under the MIT license
